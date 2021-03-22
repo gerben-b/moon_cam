b5335579c26b80fbdde8be97eabc5baee756a454
gerben-b <53979644+gerben-b@users.noreply.github.com>
2021-03-22 20:51:36 +0100

    Delete scope_slew_west.sh

--- scope_slew_west.sh
+++
@@ -1,4 +0,0 @@
-indi_setprop "EQMod Mount.TELESCOPE_SLEW_RATE.1x=On"
-indi_setprop "EQMod Mount.TELESCOPE_MOTION_WE.MOTION_WEST=On"
-sleep 1
-indi_setprop "EQMod Mount.TELESCOPE_MOTION_WE.MOTION_WEST=Off"
