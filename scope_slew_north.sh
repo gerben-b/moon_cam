f9dbff2a36fdaa3a7c5fb421cf67956d547d8426
gerben-b <53979644+gerben-b@users.noreply.github.com>
2021-03-22 20:47:27 +0100

    Add files via upload

---
+++ scope_slew_north.sh
@@ -0,0 +1,4 @@
+indi_setprop "EQMod Mount.TELESCOPE_SLEW_RATE.1x=On"
+indi_setprop "EQMod Mount.TELESCOPE_MOTION_NS.MOTION_NORTH=On"
+sleep 1
+indi_setprop "EQMod Mount.TELESCOPE_MOTION_NS.MOTION_NORTH=Off"
