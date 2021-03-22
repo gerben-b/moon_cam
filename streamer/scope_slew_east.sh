7500d92f0a57d69e050929db8c21d699058d2f86
gerben-b <53979644+gerben-b@users.noreply.github.com>
2021-03-22 20:48:26 +0100

    Add files via upload

---
+++ streamer/scope_slew_east.sh
@@ -0,0 +1,4 @@
+indi_setprop "EQMod Mount.TELESCOPE_SLEW_RATE.1x=On"
+indi_setprop "EQMod Mount.TELESCOPE_MOTION_WE.MOTION_EAST=On"
+sleep 1
+indi_setprop "EQMod Mount.TELESCOPE_MOTION_WE.MOTION_EAST=Off"
---
+++ streamer/scope_track_off.sh
@@ -0,0 +1,2 @@
+indi_setprop "EQMod Mount.TELESCOPE_TRACK_STATE.TRACK_OFF=On"
+indi_setprop "EQMod Mount.CONNECTION.DISCONNECT=On"
